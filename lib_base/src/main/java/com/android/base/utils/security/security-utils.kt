08c863db3b41d234dd41546b682664578cfaca10
Ztiany <Ztiany3@gmail.com>
2019-11-01 11:38:43 +0800

    remove unused method

--- lib_base/src/main/java/com/android/base/utils/security/security-utils.kt
+++ lib_base/src/main/java/com/android/base/utils/security/security-utils.kt
@@ -1,12 +1,5 @@
 package com.android.base.utils.security
 
-import android.annotation.SuppressLint
-
 fun md5(content: String): String {
     return MD5Utils.md5(content) ?: ""
-}
-
-@SuppressLint("DefaultLocale")
-fun md5UpperCase(content: String): String {
-    return MD5Utils.md5(content)?.toUpperCase() ?: ""
 }
