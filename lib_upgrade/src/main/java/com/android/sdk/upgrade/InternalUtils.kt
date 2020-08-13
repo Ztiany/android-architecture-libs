a3059fc819a0876052ae27ad328f4737d2b74871
ztiany <ztiany3@gmail.com>
2020-08-13 14:08:44 +0800

    add lib_upgrade

---
+++ lib_upgrade/src/main/java/com/android/sdk/upgrade/InternalUtils.kt
@@ -0,0 +1,30 @@
+@file:JvmName("InternalUtils")
+
+package com.android.sdk.upgrade
+
+import java.io.Closeable
+import java.io.File
+
+internal fun File.makeParentPath(): Boolean {
+    val path = this.parentFile
+    return path.exists() || makeDir(path)
+}
+
+private fun makeDir(file: File?): Boolean {
+    return file != null && (file.exists() || file.mkdirs())
+}
+
+internal fun closeIOQuietly(vararg closable: Closeable?) {
+    closable.forEach {
+        if (it != null) {
+            try {
+                it.close()
+            } catch (ignore: Exception) {
+            }
+        }
+    }
+}
+
+internal fun isEmpty(str: String?): Boolean {
+    return str == null || str.isEmpty()
+}
